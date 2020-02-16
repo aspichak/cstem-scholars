29b06e4d83e46e90bedf3d053a12b85744f9f3ff
aspichak <aspichak@eagles.ewu.edu>
2020-02-16 15:49:57 -0800

    Added validation library, helper methods, composer autoloader
    - Added Respect\Validation library
    - Added get(), post(), and isPost() helper methods
    - Switched to Composer autoloader to load the DB class

--- src/includes/init.php
+++ src/includes/init.php
@@ -1,21 +1,33 @@
 <?php
 session_start();
 
-define("ROOT", __DIR__ . "/..");
+define('ROOT', __DIR__ . '/..');
 
 function path($file) {
-    return ROOT . "/" . $file;
+	return ROOT . '/' . $file;
 }
 
-require_once path("config.php");
-require_once path("includes/database.php");
+require_once path('config.php');
+require_once path('vendor/autoload.php');
 
 DB::configure(DB_CONNECTION_STRING, DB_USERNAME, DB_PASSWORD, NULL);
 
 function authorize($role) {
-    if (!isset($_SESSION) || $_SESSION["role"] != $role) {
-        http_response_code(401);
-        require path("infoPages/unauthorized.php");
-        exit();
-    }
+	if (!isset($_SESSION) || $_SESSION['role'] != $role) {
+		http_response_code(401);
+		require path('infoPages/unauthorized.php');
+		exit();
+	}
+}
+
+function isPost() {
+	return $_SERVER['REQUEST_METHOD'] == 'POST';
+}
+
+function get($param, $default = NULL) {
+	return $_GET[$param] ?? $default;
+}
+
+function post($param, $default = NULL) {
+	return $_POST[$param] ?? $default;
 }
