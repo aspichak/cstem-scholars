3784294e8466531168b0c543be0450083d2b039c
aspichak <aspichak@eagles.ewu.edu>
2020-02-20 23:08:00 -0800

    Fixed broken styles

--- src/Admin/index.php
+++ src/Admin/index.php
@@ -3,9 +3,8 @@
 <head>
     <title>Admin</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php
     $database = parse_ini_file("config.ini");
     $host = $database['host'];
