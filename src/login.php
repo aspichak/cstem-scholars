df7ab1de24f56b54ba4f8266f40e7e6399816fad
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-05-07 18:46:25 -0700

    login fixup for null return of period

--- src/login.php
+++ src/login.php
@@ -123,59 +123,75 @@ if (defined('DEBUG') && DEBUG) {
         if ($user->isAdmin) {
             header("location:/Admin/index.php");
         } else {
-            error('Admin',
-                  'You are not an admin for the CSTEM Undergraduate Research Grant.',
-            401);
+            error(
+                'Admin',
+                'You are not an admin for the CSTEM Undergraduate Research Grant.',
+                401
+            );
         }
     } elseif ($type == 'student') {
         // STUDENT PAGE LOGIN
         if (!$user->isStudent) {
-            error('Student Application',
-                  'You are not an EWU student and are not eligible to apply for the CSTEM Research Grant.',
-            401);;
-        } elseif (Period::current() != false) {
+            error(
+                'Student Application',
+                'You are not an EWU student and are not eligible to apply for the CSTEM Research Grant.',
+                401
+            );;
+        } elseif (Period::current() != null) {
             header("location:/students");
         } else {
-            error('Student Application',
-                  'The CSTEM Research Grant application has been closed. Please check back at a later date.',
-            403);
+            error(
+                'Student Application',
+                'The CSTEM Research Grant application has been closed. Please check back at a later date.',
+                403
+            );
         }
     } elseif ($type == 'advisor') {
         // ADVISOR PAGE LOGIN
         if ($user->isStudent) {
-            error('Advisor Approval',
-            'You are not a faculty advisor for Eastern\'s CSTEM research grant.',
-            401);
-        } elseif (Period::currentForAdvisors() != false) {
+            error(
+                'Advisor Approval',
+                'You are not a faculty advisor for Eastern\'s CSTEM research grant.',
+                401
+            );
+        } elseif (Period::currentForAdvisors() != null) {
             if ($user->isAdvisor) {
                 header("location:faculty/facultylandingpage.php");
             } else {
-                error('Advisor Approval',
-                'Currently there are no applications needing approval. Please check back later.',
-                204);
+                error(
+                    'Advisor Approval',
+                    'Currently there are no applications needing approval. Please check back later.',
+                    204
+                );
             }
         } else {
-            error('Advisor Approval',
-                  'The deadline has passed for approving students applications.',
-                  204);
+            error(
+                'Advisor Approval',
+                'The deadline has passed for approving students applications.',
+                204
+            );
         }
     } elseif ($type == "reviewer") {
         // REVIEWER PAGE LOGIN
         if ($user->isStudent) {
-            error('Application Reviewal',
-                  'You are not a reviewer for Eastern\'s CSTEM Research Grant application.',
-            401);
+            error(
+                'Application Reviewal',
+                'You are not a reviewer for Eastern\'s CSTEM Research Grant application.',
+                401
+            );
         }
         if ($user->isReviewer) {
-            if (Period::currentForAdvisors()!=false) {
+            if (Period::currentForAdvisors() != null) {
                 header("location:/reviewers/ReviewStudents.php");
             } else {
                 header("location:infoPages/closedReviewer.php");
             }
         } else {
-            error('Application Reviewal',
-                  'You are not a reviewer for Eastern\'s CSTEM Research Grant application.',
-            401);
+            error(
+                'Application Reviewal',
+                'You are not a reviewer for Eastern\'s CSTEM Research Grant application.',
+                401
+            );
         }
     }
 }
