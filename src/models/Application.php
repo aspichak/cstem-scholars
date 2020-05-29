59d634cf25d4501205060ecafb81184b2701c114
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-05-28 23:33:45 -0700

    and forgot to commit files

--- src/models/Application.php
+++ src/models/Application.php
@@ -76,6 +76,11 @@ class Application extends Model
         return parent::all($query, ...$params);
     }
 
+    public static function forAdvisor($advisor)
+    {
+        return Application::all('advisorEmail = ? AND status = submitted', $advisor);
+    }
+
     public function fill($form, $fillGuardedColumns = [])
     {
         $this->hasAgreedToTerms = (($form['terms'] ?? '') == 'agree');
