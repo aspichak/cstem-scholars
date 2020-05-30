3ecf01932207dac856af6758968066a94f43365b
Alexander Spichak <aspichak@eagles.ewu.edu>
2020-05-30 15:36:20 -0700

    New admin layout design

--- src/templates/admin/user.php
+++ src/templates/admin/user.php
@@ -4,24 +4,27 @@ $title = 'User Form';
 $layout = 'admin/_layout.php';
 ?>
 
-<h1>User</h1>
+<form class="panel" method="POST">
+    <h1>User</h1>
 
-<?php
-if ($form->errors()) {
-    echo $form->errors();
-}
-?>
-
-<form method="POST">
     <?= $form->csrf() ?>
 
-    <label>Name: <?= $form->text('name', ['required']) ?></label>
-    <label>Email: <?= $form->email('email', ['required']) ?></label>
+    <div class="form-group">
+        <label for="name">Name:</label>
+        <?= $form->text('name', ['required']) ?>
+    </div>
 
-    <p>Roles:</p>
-    <label><?= $form->checkbox('isAdvisor') ?> Advisor</label><br>
-    <label><?= $form->checkbox('isReviewer') ?> Reviewer</label><br>
-    <label><?= $form->checkbox('isAdmin') ?> Administrator</label><br>
+    <div class="form-group">
+        <label for="email">Email:</label>
+        <?= $form->email('email', ['required']) ?>
+    </div>
+
+    <div class="form-group">
+        <p>Roles:</p>
+        <label><?= $form->checkbox('isAdvisor') ?> Advisor</label><br>
+        <label><?= $form->checkbox('isReviewer') ?> Reviewer</label><br>
+        <label><?= $form->checkbox('isAdmin') ?> Administrator</label><br>
+    </div>
 
     <button type="submit">Submit</button>
 </form>
