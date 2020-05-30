8d7ec1f9affc62baf07657512347f6f1523608e7
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-05-29 22:23:35 -0700

    warg

--- src/public/advisors/applications.php
+++ src/public/advisors/applications.php
@@ -7,7 +7,7 @@ User::authorize('advisor');
 $c = new ModelController(Application::class);
 
 // TODO: Filter applications by period, student name, email, status, etc.
-$c->index('admin/applications.php', ['applications' => Application::all()]);
+$c->index('advisors/applications.php', ['applications' => Application::all()]);
 $c->read();
 
 if ($c->action() == 'update') {
