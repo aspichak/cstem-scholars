9cc7b798820b0097d8ba3490736ab4e194a38d86
Jonathyn Komorita <iamjonkomo@gmail.com>
2020-05-31 23:32:42 -0700

    New dashboard showing up and displaying correct reviewer options

---
+++ src/public/reviewers/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../../init.php';
+
+User::authorize('reviewer');
+
+echo HTML::template('reviewer/index_layout.php');
+
+
---
+++ src/templates/reviewer/index_layout.php
@@ -0,0 +1,9 @@
+<?php
+
+$title = 'Reviewer Dashboard';
+$layout = 'reviewer/_layout.php';
+?>
+
+<h1>Reviewer Dashboard</h1>
+
+<p>This page will display more later</p>
