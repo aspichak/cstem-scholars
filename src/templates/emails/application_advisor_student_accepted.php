8fa4e4e4d0db454108f31eb2495a64a5c3bded4f
aspichak <aspichak@eagles.ewu.edu>
2020-06-03 21:35:37 -0700

    Update application_advisor_student_accepted.php

--- src/templates/emails/application_advisor_student_accepted.php
+++ src/templates/emails/application_advisor_student_accepted.php
@@ -5,7 +5,7 @@ $layout = 'emails/layout.php';
 
 <p>Hello <?= e($application->name) ?>, </p>
 <p>
-    Your application: <?= e($application->title) ?> for <?= e($period->deadline) ?> has been forward for review.
+    Your application: <?= e($application->title) ?> for <?= e($period->deadline) ?> has been forwarded for review.
     This does -not- mean it has been approved, it is still under review.
 </p>
 <?= $studentComment ?>
