2d3553a102f6ccf09393dd5c7d833b87bab7c418
Alexander Spichak <aspichak@eagles.ewu.edu>
2020-06-05 00:50:55 -0700

    Added tab JS

--- src/templates/admin/application.php
+++ src/templates/admin/application.php
@@ -6,7 +6,7 @@ $layout = 'admin/_layout.php';
 
 <h1><?= e($application->title) ?></h1>
 
-<?php if ($error) echo tag('div', $error, ['class' => 'error']) ?>
+<?php if ($error) echo tag('div', $error, ['class' => 'message error']) ?>
 
 <ul class="tabs">
     <li><a class="active" href="#award">Award</a></li>
