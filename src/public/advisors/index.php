66a255d7ceb3169eae6de62a24b6edbd6ec57965
Alexander Spichak <aspichak@eagles.ewu.edu>
2020-06-09 15:05:25 -0700

    Deleted unused advisors/index.php page

--- src/public/advisors/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once '../../init.php';
-
-User::authorize('advisor');
-
-echo HTML::template('advisors/index.php');
--- src/templates/advisors/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$title = 'Advisor Dashboard';
-$layout = 'admin/_layout.php';
-?>
-
-<h1>Advisor Dashboard</h1>
-
-<p>This page shows applications (filling in more later)</p>
