197100873bdf8af03b1be0c4703d8f2b3f293a58
Jeffrey Nix <jnix1@eagles.ewu.edu>
2020-06-09 20:52:07 -0700

    cleaned up more

--- src/public/students/thank_you.php
+++ src/public/students/thank_you.php
@@ -11,7 +11,7 @@ $period = Period::current();
     <title>Thank you!</title>
     <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <link rel="stylesheet" href="css/students.css">
+    <link rel="stylesheet" href="../CSS/students.css">
 </head>
 
 <body>
