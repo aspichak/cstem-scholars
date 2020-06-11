5319cc13d864fccbc4fddbbe055f8a1b04cfc092
Alexander Spichak <aspichak@eagles.ewu.edu>
2020-06-11 16:43:25 -0700

    Added missing <head>

--- src/public/index.php
+++ src/public/index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<title>Research Home Page</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="CSS/index.css">
-<link rel="icon" href="favicon.png"/>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<head>
+    <title>Research Home Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="CSS/index.css">
+    <link rel="icon" href="favicon.png" />
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+</head>
 <body>
 
 <!-- Navbar (sit on top) -->
